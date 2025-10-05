d7761930e9b327de16f698f9c6445ee5d957b76e
dvdaines <ddaines@protonmail.com>
2025-10-05 16:32:29 -0600

    enable vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata, Viewport } from "next";
+import { Analytics } from "@vercel/analytics/next"
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -78,6 +79,7 @@ export default function RootLayout({
     <html lang="en" className="bg-black">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
+        <Analytics />
       </body>
     </html>
   );
