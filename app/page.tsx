af92b54562595f520a85302bb559e27a0fdecb5c
dvdaines <ddaines@protonmail.com>
2025-10-04 20:28:37 -0600

    update homepage

--- app/page.tsx
+++ app/page.tsx
@@ -135,7 +135,7 @@ export default function Dashboard() {
               ) : (
                 <iframe
                   className="w-full h-full"
-                  src="https://www.youtube.com/watch?v=1FK4x84fls8?autoplay=1"
+                  src="https://www.youtube.com/embed/1FK4x84fls8?autoplay=1"
                   title="YouTube video player"
                   frameBorder="0"
                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
