ddca1a191a7a6f2c6f27c0503d9f0b09ee0449ba
David <33950096+dvdaines@users.noreply.github.com>
2025-10-27 22:17:57 -0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -78,7 +78,7 @@ export default function Home() {
                 </tr>
                 <tr>
                   <th scope="row">Data</th>
-                  <td>Open data: 2 months pre (baseline), 12 months during, 2 month post</td>
+                  <td>Open data: 2 months pre (baseline), 12 months during, 2 months post</td>
                 </tr>
               </tbody>
             </table>
